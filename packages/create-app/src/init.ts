b3bf2ff479848545c328741fcff8a58fdbebaa49
LjKBurN <347156843@qq.com>
2022-02-25 17:58:53 +0800

    create-app v0.0.5

--- packages/create-app/src/init.ts
+++ packages/create-app/src/init.ts
@@ -1,37 +1,39 @@
+#!/usr/bin/env node
+
 import * as fs from 'fs';
 import { join } from 'path';
-import * as yargs from 'yargs';
 import * as shell from 'shelljs';
 
 const logGreen = (text: string) => {
-  console.log(`\x1B[32m ${text}`)
-}
+  console.log(`\x1B[32m ${text}`);
+};
 const logRed = (text: string) => {
-  console.log(`\x1B[31m ${text}`)
-}
+  console.log(`\x1B[31m ${text}`);
+};
 
-yargs
-  .command('init [projectName]', 'init a new project', {}, (argv) => {
-    const cwd = process.cwd();
-    let projectName;``
-    if (argv.projectName) {
-      projectName = argv.projectName as string;
-    } else {
-      logRed('未指定项目名称');
-      return;
-    }
+const init = async () => {
+  const cwd = process.cwd();
+  const projectName = process.argv[2];
+  if (!projectName) {
+    logRed('未指定项目名称');
+    return;
+  }
+  if (fs.existsSync(projectName)) {
+    logRed(`${projectName} already existed, please delete it`);
+    return;
+  }
 
-    if (fs.existsSync(projectName)) {
-      logRed(`${projectName} already existed, please delete it`);
-      return;
-    }
+  shell.cp(
+    '-r',
+    `${join(__dirname, `../template`)}`,
+    `${join(cwd, `./${projectName}`)}`
+  );
 
-    shell.cp('-r', `${join(__dirname, `../template`)}`, `${join(cwd, `./${projectName}`)}`);
+  logGreen(`${projectName} has created succeed `);
 
-    logGreen(`${projectName} has created succeed `)
+  console.log(`  cd ${projectName}`);
+  console.log('  npm install (or `yarn`)');
+  console.log('  npm start (or `yarn start`)');
+};
 
-    console.log(`  cd ${projectName}`)
-    console.log('  npm install (or `yarn`)')
-    console.log('  npm start (or `yarn start`)')
-  })
-  .argv
+export { init };
